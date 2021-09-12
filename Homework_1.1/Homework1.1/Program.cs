3e10d85e746cfbc9849afe160624975715cb8f97
Александра <@LAPTOP-93DTVP32>
2021-09-12 20:41:16 +0300

    Добавьте файлы проекта.

---
+++ Homework_1.1/Homework1.1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Homework1._1
+{
+    class Task01
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+    class Task02
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("++++++    ++++++++  ++++++++++  ++++++    ++++++++  ++        ++      ++");
+            Console.WriteLine("++    ++  ++            ++      ++    ++  ++    ++   ++      ++      ++++   ");
+            Console.WriteLine("++    ++  ++            ++      ++    ++  ++    ++    ++    ++      ++  ++  ");
+            Console.WriteLine("++++++    ++++++++      ++      ++++++    ++    ++    ++    ++     ++    ++");
+            Console.WriteLine("++        ++            ++      ++  ++    ++    ++     ++  ++      ++++++++  ");
+            Console.WriteLine("++        ++            ++      ++   ++   ++    ++       +++       ++    ++    ");
+            Console.WriteLine("++        ++++++++      ++      ++    ++  ++++++++        +        ++    ++");
+        }
+    }
+}
+
